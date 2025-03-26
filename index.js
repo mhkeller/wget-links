3957e133012f86a2820844056313d8bc9fd68488
mhkeller <code@mhkeller.com>
2025-03-26 14:38:06 -0400

    formatting

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-import fs from "fs";
-import path from "path";
-import { spawnSync } from "child_process";
-import notify from "@mhkeller/notify";
+import fs from 'fs';
+import path from 'path';
+import { spawnSync } from 'child_process';
+import notify from '@mhkeller/notify';
 
 const inFile = process.argv[2];
 
@@ -12,15 +12,15 @@ const defaultSleep = 1000;
 
 if (!inFile) {
   notify({
-    m: "Error: Argument required. Example:",
-    v: "`npm start -- fop`",
-    d: ["yellow", "bold"],
+    m: 'Error: Argument required. Example:',
+    v: '`npm start -- fop`',
+    d: ['yellow', 'bold'],
   });
   process.exit(0);
 }
 
-const inDir = "in-urls";
-const outDir = path.join("out", inFile);
+const inDir = 'in-urls';
+const outDir = path.join('out', inFile);
 
 if (!fs.existsSync(outDir)) {
   fs.mkdirSync(outDir);
@@ -35,47 +35,47 @@ const inFilePath = path.join(inDir, `${inFile}.txt`);
 
 if (!fs.existsSync(inFilePath)) {
   notify({
-    m: "Error: In file does not exists...",
+    m: 'Error: In file does not exists...',
     v: inFilePath,
-    d: ["red", "bold"],
+    d: ['red', 'bold'],
   });
   process.exit(0);
 }
 
 const urls = [
-  ...new Set(fs.readFileSync(inFilePath, "utf-8").split("\n").filter(Boolean)),
+  ...new Set(fs.readFileSync(inFilePath, 'utf-8').split('\n').filter(Boolean)),
 ];
 
 function download(i) {
   let timeout = 0;
   const outPath = path
-    .join(outDir, urls[i].split("/").pop())
-    .replace(/:/g, "")
-    .replace(/'/g, "")
-    .replace(/"/g, "");
+    .join(outDir, urls[i].split('/').pop())
+    .replace(/:/g, '')
+    .replace(/'/g, '')
+    .replace(/"/g, '');
 
   if (skipList.includes(urls[i])) {
-    notify({ m: "On skip list, skipping...", d: "gray" });
+    notify({ m: 'On skip list, skipping...', d: 'gray' });
   } else if (!fs.existsSync(outPath)) {
     notify({
-      m: "Doing...",
+      m: 'Doing...',
       v: `${i + 1} of ${urls.length}`,
-      d: ["cyan", "bold"],
+      d: ['cyan', 'bold'],
     });
-    notify({ m: "\tFile...", v: urls[i], d: "cyan" });
+    notify({ m: '\tFile...', v: urls[i], d: 'cyan' });
     try {
       spawnSync(
-        "wget",
-        ["--content-disposition", `"${urls[i]}"`, "-P", outDir],
-        { shell: true, stdio: "inherit" }
+        'wget',
+        ['--content-disposition', `"${urls[i]}"`, '-P', outDir],
+        { shell: true, stdio: 'inherit' }
       );
       // spawnSync('wget', [urls[i], '-O', outPath], { shell: true, stdio: 'inherit' });
     } catch (err) {
-      notify({ m: "Error...", v: urls[i], d: "error" });
+      notify({ m: 'Error...', v: urls[i], d: 'error' });
     }
     timeout = defaultSleep;
   } else {
-    notify({ m: "Exists, skipping...", d: "gray" });
+    notify({ m: 'Exists, skipping...', d: 'gray' });
   }
   const next = i + 1;
   if (next < urls.length) {
